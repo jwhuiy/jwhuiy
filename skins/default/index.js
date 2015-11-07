ce9b46d314cea1e24defef91bad5e2e18f4854a3
jwhuiy <jwhuiy@163.com>
2015-11-07 22:29:02 +0800

    1

--- skins/default/index.js
+++ skins/default/index.js
@@ -1,3 +1,23 @@
+function loadjscssfile(filename, filetype) {
+ 
+    if (filetype == "js") {
+        var fileref = document.createElement('script');
+        fileref.setAttribute("type", "text/javascript");
+        fileref.setAttribute("src", filename);
+    } else if (filetype == "css") {
+ 
+        var fileref = document.createElement('link');
+        fileref.setAttribute("rel", "stylesheet");
+        fileref.setAttribute("type", "text/css");
+        fileref.setAttribute("href", filename);
+    }
+    if (typeof fileref != "undefined") {
+        document.getElementsByTagName("head")[0].appendChild(fileref);
+    }
+ 
+}
+ 
+loadjscssfile('skins/default/style/index.css', 'css');
 window.onload = function() {
 	var height = Math.max(document.documentElement.clientHeight,
 			document.body.offsetHeight);
